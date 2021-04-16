b729bb09b9b85040a1a18f5ac63a5a1cdc55c865
Guillaume Wenzek <guw@fb.com>
2021-04-16 19:09:54 +0200

    use python 3.3

--- sublime_logging.py
+++ sublime_logging.py
@@ -2,15 +2,17 @@ import logging
 import logging.config
 import os
 import sys
-from typing import Dict, List, Optional, TextIO
 
 import sublime
 from sublime_lib.st3 import sublime_lib
+
+VERSION = "{}{}".format(sys.version_info.major, sys.version_info.minor)
 
 
 class OutputPanelHandler(logging.StreamHandler):
     @staticmethod
     def create_panel(name: str):
+        name += "_" + VERSION
         window = sublime.active_window()
         # Try to append to the existing panel instead of creating a new one.
         view = window.find_output_panel(name)
@@ -24,42 +26,53 @@ class OutputPanelHandler(logging.StreamHandler):
 
 
 class SnitchingStdout:
-    def __init__(self, console: TextIO):
+    def __init__(self, console):
         self.console = console
-        self.logger = logging.getLogger("SublimeLoggingSnitch")
+        self.logger = logging.getLogger("SublimeLoggingSnitch_{}".format(VERSION))
         self.logger.warning(
-            f"Stdout will first go through {self} before going to {self.console}"
+            "Stdout will first go through {} before going to {}", (self, self.console)
         )
-        self._buffer: List[str] = []
+        self._buffer = []
 
-    def get_line(self, text: str) -> str:
+    def get_message(self, text: str) -> str:
+        # TODO: this can still insert newlines when not desired.
         self._buffer.append(text)
-        if os.linesep not in text:
+        if "\n" not in text:
             return ""
         line = "".join(self._buffer)
         self._buffer = []
-        return line
+        return line.rstrip("\n")
 
     def write(self, text: str) -> int:
-        # Add options to filter _console content
+        # TODO: Add options to filter console content
         # n = self.console.write(text.replace("\n", " ✓\n"))
         n = self.console.write(text)
 
-        line = self.get_line(text)
-        if not line:
+        message = self.get_message(text)
+        if not message:
             return n
         frame = sys._getframe(1)
-        if frame is not None:
-            caller = frame.f_code.co_filename
-        if caller in "<string>":
-            # Called from the console REPL
-            # TODO: try to also skip writes from the logging module
+        caller = frame.f_code.co_filename
+        if caller == "<string>" or caller.endswith("/logging/__init__.py"):
+            # * caller == <string> means printing from the console REPL
+            # * don't snitch on logging calls.
+            print("won't snitch on [{}]".format(caller), file=self.console)
             return n
-        self.logger.info(line.rstrip(os.linesep))
+        print("\nwill snitch on [{}] [{}]".format(caller, message), file=self.console)
+        self.logger.info(message)
         return n
 
     def flush(self):
-        self._stdout.flush()
+        self.console.flush()
+
+
+def to_dict(settings: sublime.Settings) -> dict:
+    keys = ["version", "formatters", "handlers", "root", "loggers"]
+    d = {key: settings.get(key) for key in keys}
+    if VERSION == "38":
+        print(settings.to_dict())
+    print(d)
+    return d
 
 
 def setup_logging(settings: sublime.Settings = None):
@@ -70,7 +83,7 @@ def setup_logging(settings: sublime.Settings = None):
 
     logger = logging.getLogger("SublimeLogging")
     logger.warning("Logging config will be reseted !")
-    logging.config.dictConfig(settings.to_dict())
+    logging.config.dictConfig(to_dict(settings))
     logger.warning("Logging has been setup !")
 
 
@@ -78,7 +91,7 @@ def plugin_loaded():
     setup_logging()
     logger = logging.getLogger("SublimeLogging")
 
-    console: Optional[TextIO] = sys.stdout
+    console = sys.stdout
     if not isinstance(console, sublime._LogWriter):
         # This happens when hot reloading the package
         console = getattr(sys.stdout, "console", None)
@@ -87,13 +100,17 @@ def plugin_loaded():
                 "Wasn't able to identify the Sublime console object, "
                 "Snitching is disabled. Try restarting ST."
             )
-            logger.error(f"{sys.stdout=}, {sys.__stdout__=}")
+            logger.error("sys.stdout={}, sys.__stdout={}", sys.stdout, sys.__stdout__)
             return
 
     snitch = SnitchingStdout(console)
     logger.info(
-        f"Stdout will first go through {snitch} before going to {snitch._stdout}",
-        file=snitch._stdout,
+        "Stdout will first go through {} before going to {}",
+        snitch,
+        snitch.console,
     )
+    print("Loggers:", {k: v.disabled for k, v in logging.root.manager.loggerDict.items()})
+    for k, v in logging.root.manager.loggerDict.items():
+        v.disabled = False
     sys.stdout = snitch
-    print("this should be snitched")
+    print("this should be snitched on")
